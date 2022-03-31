db49458b18f848df8863bfa421d675de21855662
Jeison <jeiison.rodriguez7@gmail.com>
2022-03-31 13:38:51 -0500

    first commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,16 @@
+import "../styles/Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className="sk-chase px-6 items-center">
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+</div>
+  )
+}
+
+export default Spinner
